0a2900b641650e1bbcf27f38383a47b1a88c2ae4
milaaaadh <mahdioohabibi@gmail.com>
2021-08-10 01:06:36 +0430

    homework5

---
+++ main.py
@@ -0,0 +1,19 @@
+def fun1(*values):
+    """print all value"""
+    for value in values:
+        print(value)
+
+def calculation(a,b):
+    """print sum and minus"""
+    return a+b , max(a,b)-min(a,b)
+
+def showEmployee(name, salary=9000):
+    print("Employee", name, "salary is: ", salary)
+
+
+def displayStudent(name, age):
+    print("{} is {} years old".format(name,age))
+
+showStudent = displayStudent
+showStudent("mahdi", 25)
+
